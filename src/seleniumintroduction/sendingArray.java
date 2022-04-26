90da642f92b41e6c14dc1818dc59fb1e7ff4e6b4
MounikaUpputuri28081993 <mouni.upputuri@gmail.com>
2022-04-26 23:09:28 +0530

    java course

--- src/seleniumintroduction/sendingArray.java
+++ src/seleniumintroduction/sendingArray.java
@@ -3,6 +3,7 @@ package seleniumintroduction;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,28 +12,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class sendingArray {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "D:/selenium practice/chromedriver_win32/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		String[] veg= {"Cucumber","Brocolli"};
+		String[] veg = { "Cucumber", "Brocolli" };
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
-		Thread.sleep(3000);
 		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
-		for (int i = 0; i < products.size(); i++) 
-		{
+		for (int i = 0; i < products.size(); i++) {
 			String[] Name = products.get(i).getText().split("-");
-			String Final=Name[0].trim();
-			List veg1= Arrays.asList(veg);
-			
-			if (veg1.contains(Final)) 
-			{
+			String Final = Name[0].trim();
+			List veg1 = Arrays.asList(veg);
+
+			if (veg1.contains(Final)) {
 				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
-				Thread.sleep(4000);
 
 			}
 
 		}
+		driver.findElement(By.xpath("//a/img[@alt='Cart']")).click();
+		driver.findElement(By.xpath("//div/button[text()='PROCEED TO CHECKOUT']")).click();
+		driver.findElement(By.cssSelector("input.promocode")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.xpath("//button[@class='promoBtn']")).click();
+		System.out.println(driver.findElement(By.xpath("//span[@class='promoInfo']")).getText());
+		driver.findElement(By.xpath("//button[text()='Place Order']")).click();
+
 	}
 
 }
